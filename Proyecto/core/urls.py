e87d672751e7ff967561a4f8a75914ca361b7250
Javier Pozuelo Ruiz <73185569+japrvi@users.noreply.github.com>
2026-04-09 10:57:45 +0200

    Delete Proyecto directory

--- Proyecto/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Incluimos las rutas de nuestra aplicación web (capa de adaptadores / delivery)
-    path('', include('delivery.urls')),
-]
--- Proyecto/use_cases/catalogo_use_cases.py
+++
@@ -1,9 +0,0 @@
-from domain.repository_interfaces import CatalogoRepository
-
-class ConsultarCatalogoUseCase:
-    def __init__(self, catalogo_repo: CatalogoRepository):
-        self.catalogo_repo = catalogo_repo
-        
-    def execute(self):
-        # En el futuro se puede aplicar lógica extra: filtros, paginación, etc.
-        return self.catalogo_repo.listar_platos_en_carta()
