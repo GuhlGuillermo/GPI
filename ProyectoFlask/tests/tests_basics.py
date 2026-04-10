daa1296988a4f1768b762a51b02d45d2176f72c0
GuhlGuillermo <guillermoguhl23@gmail.com>
2026-04-10 10:15:11 +0200

    Add files via upload

--- ProyectoFlask/tests/tests_basics.py
+++ ProyectoFlask/tests/tests_basics.py
@@ -1,1 +1,8 @@
+from app import create_app
 
+def test_home():
+    app = create_app()
+    client = app.test_client()
+
+    response = client.get("/platos")
+    assert response.status_code in [200, 404]
